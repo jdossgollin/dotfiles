2425d25ad68afffce966b5c38df7ca62439b4760
James Doss-Gollin <james.dossgollin@gmail.com>
2017-10-02 21:52:04 +0200

    Fix dock

--- macos/dock.sh
+++ macos/dock.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 
 dockutil --no-restart --remove all
-dockutil --no-restart --add "/Applications/Google Chrome.app"
-dockutil --no-restart --add "/Applications/Mail.app"
 dockutil --no-restart --add "/Applications/Calendar.app"
+dockutil --no-restart --add "/Applications/Messages.app"
+dockutil --no-restart --add "/Applications/FaceTime.app"
+dockutil --no-restart --add "/Applications/Firefox.app"
+dockutil --no-restart --add "/Applications/Photos.app"
+dockutil --no-restart --add "/Applications/Spotify.app"
+dockutil --no-restart --add "/Applications/Rambox.app"
+dockutil --no-restart --add "/Applications/RStudio.app"
+dockutil --no-restart --add "/Applications/Papers.app"
+dockutil --no-restart --add "/Applications/Evernote.app"
 dockutil --no-restart --add "/Applications/Utilities/Terminal.app"
 dockutil --no-restart --add "/Applications/System Preferences.app"
-dockutil --no-restart --add "/Applications/Spotify.app"
 
 killall Dock
