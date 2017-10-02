b7eed2f7d6b9809642df83c01a8e3c1d3bd46bd3
James Doss-Gollin <james.dossgollin@gmail.com>
2017-10-02 11:28:04 -0400

    clean up extra lines

--- install/brew.sh
+++ install/brew.sh
@@ -48,52 +48,5 @@ brew install "${apps[@]}"
 brew cleanup
 brew doctor
 
-
-
-
-
-
-
-brew tap Goles/battery
-brew update
-brew upgrade
-
-# Install packages
-
-apps=(
-  bash-completion2
-  bats
-  battery
-  coreutils
-  diff-so-fancy
-  dockutil
-  ffmpeg
-  fasd
-  gifsicle
-  git
-  git-extras
-  gnu-sed --with-default-names
-  grep --with-default-names
-  hub
-  httpie
-  imagemagick
-  jq
-  lynx
-  mackup
-  nano
-  pandoc
-  peco
-  psgrep
-  python
-  shellcheck
-  ssh-copy-id
-  tree
-  unar
-  wget
-  wifi-password
-)
-
-brew install "${apps[@]}"
-
 export DOTFILES_BREW_PREFIX_COREUTILS=`brew --prefix coreutils`
 set-config "DOTFILES_BREW_PREFIX_COREUTILS" "$DOTFILES_BREW_PREFIX_COREUTILS" "$DOTFILES_CACHE"
