4c51cd70a7585324f20dd060d2e36caa3acbeea6
James Doss-Gollin <james.dossgollin@gmail.com>
2017-10-08 12:34:42 -0400

    Install gtop using npm

--- install.sh
+++ install.sh
@@ -24,6 +24,7 @@ ln -sfv "$DOTFILES_DIR/git/.gitignore_global" ~
 . "$DOTFILES_DIR/install/brew.sh"
 . "$DOTFILES_DIR/install/bash.sh"
 . "$DOTFILES_DIR/install/brew-cask.sh"
+. "$DOTFILES_DIR/install/npm.sh"
 
 # Run tests
 if is-executable bats; then bats test/*.bats; else echo "Skipped: tests (missing: bats)"; fi
