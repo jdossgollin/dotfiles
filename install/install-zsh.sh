aa8da5a809c0e7ad04ddb04a045bb7d984b706ee
James Doss-Gollin <james.dossgollin@gmail.com>
2018-05-14 21:22:55 -0700

    use z for navigation

--- install/install-zsh.sh
+++ install/install-zsh.sh
@@ -9,3 +9,13 @@ curl -L http://install.ohmyz.sh | sh
 
 # Syntax highlighting
 brew install zsh-syntax-highlighting
+
+# Powerline fonts -- needed for the theme of choice
+git clone https://github.com/powerline/fonts.git --depth=1
+cd fonts
+./install.sh
+cd ..
+rm -rf fonts
+
+# install z for completion
+brew install z
--- runcom/.zshrc
+++ runcom/.zshrc
@@ -17,6 +17,9 @@ else
   return
 fi
 
+# for z navigation
+. `brew --prefix`/etc/profile.d/z.sh
+
 # Set name of the theme to load. Optionally, if you set this to "random"
 # it'll load a random theme each time that oh-my-zsh is loaded.
 # See https://github.com/robbyrussell/oh-my-zsh/wiki/Themes
