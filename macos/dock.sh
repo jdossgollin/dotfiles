48a94909fe1ac0ce20751b9d3e64a47701ba0582
James Doss-Gollin <james.dossgollin@gmail.com>
2018-06-18 11:01:03 -0400

    put email in dock; remove slack

--- macos/dock.sh
+++ macos/dock.sh
@@ -4,7 +4,7 @@ dockutil --no-restart --remove all
 dockutil --no-restart --add "/Applications/FaceTime.app"
 dockutil --no-restart --add "/Applications/Messages.app"
 dockutil --no-restart --add "/Applications/WhatsApp.app"
-dockutil --no-restart --add "/Applications/Slack.app"
+dockutil --no-restart --add "/Applications/Kiwi for Gmail.app"
 dockutil --no-restart --add "/Applications/Photos.app"
 dockutil --no-restart --add "/Applications/Spotify.app"
 dockutil --no-restart --add "/Applications/iTunes.app"
