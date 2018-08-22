5a22b5100ee288889505c85ee1b4b1acf7a38ccd
James Doss-Gollin <james.dossgollin@gmail.com>
2018-08-22 16:03:53 -0400

    Move some apps around on the dock

--- macos/dock.sh
+++ macos/dock.sh
@@ -2,12 +2,13 @@
 
 dockutil --no-restart --remove all
 dockutil --no-restart --add "/Applications/System Preferences.app"
-dockutil --no-restart --add "/Applications/Messages.app"
-dockutil --no-restart --add "/Applications/WhatsApp.app"
-dockutil --no-restart --add "/Applications/Mail.app"
+dockutil --no-restart --add "/Applications/Photos.app"
 dockutil --no-restart --add "/Applications/Reminders.app"
 dockutil --no-restart --add "/Applications/Notes.app"
-dockutil --no-restart --add "/Applications/Photos.app"
+dockutil --no-restart --add "/Applications/Calendar.app"
+dockutil --no-restart --add "/Applications/Mail.app"
+dockutil --no-restart --add "/Applications/Messages.app"
+dockutil --no-restart --add "/Applications/WhatsApp.app"
 dockutil --no-restart --add "/Applications/Spotify.app"
 dockutil --no-restart --add "/Applications/Firefox.app"
 dockutil --no-restart --add "/Applications/Papers.app"
