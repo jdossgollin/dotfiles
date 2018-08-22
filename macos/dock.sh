3fff1d5542fc6ddf984bee8073f2c7e5ea47b8a1
James Doss-Gollin <james.dossgollin@gmail.com>
2018-08-22 11:48:21 -0400

    change the dock

--- macos/dock.sh
+++ macos/dock.sh
@@ -1,21 +1,19 @@
 #!/bin/sh
 
 dockutil --no-restart --remove all
-dockutil --no-restart --add "/Applications/FaceTime.app"
+dockutil --no-restart --add "/Applications/System Preferences.app"
 dockutil --no-restart --add "/Applications/Messages.app"
 dockutil --no-restart --add "/Applications/WhatsApp.app"
-dockutil --no-restart --add "/Applications/Kiwi for Gmail.app"
+dockutil --no-restart --add "/Applications/Mail.app"
+dockutil --no-restart --add "/Applications/Reminders.app"
+dockutil --no-restart --add "/Applications/Notes.app"
 dockutil --no-restart --add "/Applications/Photos.app"
 dockutil --no-restart --add "/Applications/Spotify.app"
-dockutil --no-restart --add "/Applications/iTunes.app"
 dockutil --no-restart --add "/Applications/Firefox.app"
 dockutil --no-restart --add "/Applications/Papers.app"
 dockutil --no-restart --add "/Applications/GitHub Desktop.app"
-dockutil --no-restart --add "/Applications/System Preferences.app"
 dockutil --no-restart --add "/Applications/iTerm.app"
-
 dockutil --no-restart --add "/Users/james/Documents/"
-dockutil --no-restart --add "/Users/james/Documents/Github/"
 dockutil --no-restart --add "/Users/james/Downloads/"
 
 killall Dock
