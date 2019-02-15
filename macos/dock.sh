3562b8dde68d80aba78c3e26dc445fe921532b1d
James Doss-Gollin <james.dossgollin@gmail.com>
2019-02-15 11:40:06 -0500

    Remove more distractions from dock

--- macos/dock.sh
+++ macos/dock.sh
@@ -2,17 +2,15 @@
 
 dockutil --no-restart --remove all
 dockutil --no-restart --add "/Applications/System Preferences.app"
-dockutil --no-restart --add "/Applications/Photos.app"
 dockutil --no-restart --add "/Applications/Calendar.app"
-dockutil --no-restart --add "/Applications/News.app"
 dockutil --no-restart --add "/Applications/Spotify.app"
 dockutil --no-restart --add "/Applications/Firefox.app"
 dockutil --no-restart --add "/Applications/Zotero.app"
-dockutil --no-restart --add "/Applications/Evernote.app"
 dockutil --no-restart --add "/Applications/GitHub Desktop.app"
 dockutil --no-restart --add "/Applications/Visual Studio Code.app"
 dockutil --no-restart --add "/Applications/iTerm.app"
-dockutil --no-restart --add "/Users/james/Documents/"
+dockutil --no-restart --add "/Users/james/Google Drive/"
+dockutil --no-restart --add "/Users/james/Documents/GitHub/"
 dockutil --no-restart --add "/Users/james/Downloads/"
 
 killall Dock
