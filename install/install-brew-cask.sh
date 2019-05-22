1208ec2029d9bf7bdcec4af3811734394a3cb2f0
James Doss-Gollin <james.dossgollin@gmail.com>
2019-05-22 12:00:21 +0100

    use rambox again, its looking much better

--- install/install-brew-cask.sh
+++ install/install-brew-cask.sh
@@ -10,17 +10,15 @@ apps=(
   adobe-acrobat-reader    # a standard PDF reader
   dashlane                # password manager
   docker                  # can be useful
-  evernote                # note taking
   firefox-beta            # keep it up to date!
   github                  # can be helpful, I suck at git
   google-backup-and-sync  # free storage from columbia
   iterm2                  # superior terminal
   mactex                  # latex!
   miniconda               # package manager
-  skype                   # can't quite get by w/o this app
+  rambox                  # email, whatsapp, and more
   spotify                 # why not
   visual-studio-code      # code editor
-  whatsapp                # also doesn't play well with Rambox
   zotero                  # reference manager
 )
 brew cask install "${apps[@]}"
