0b38ff7a14939ed862096bdbd0ed6a4194bd3b65
James Doss-Gollin <james.dossgollin@gmail.com>
2020-04-12 12:20:41 -0400

    save screenshots to special folder

--- install/install-docker.sh
+++ install/install-docker.sh
@@ -1,5 +1,8 @@
 brew cask install docker
 
+open /Applications/Docker.app
+sleep 60
+
 docker pull tianon/latex:latest
 docker pull pandoc/latex:latest
 docker pull jekyll/jekyll:3.8
--- macos/defaults.sh
+++ macos/defaults.sh
@@ -132,7 +132,7 @@ defaults write com.apple.screensaver askForPassword -int 1
 defaults write com.apple.screensaver askForPasswordDelay -int 0
 
 # Save screenshots to the downloads folder
-defaults write com.apple.screencapture location -string "${HOME}/Downloads"
+defaults write com.apple.screencapture location -string "${HOME}/Downloads/Screenshots"
 
 # Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)
 defaults write com.apple.screencapture type -string "png"
