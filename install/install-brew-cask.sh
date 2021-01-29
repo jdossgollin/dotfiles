a105388ee53187f00149623bf34c625d768d92e2
James Doss-Gollin <james.doss-gollin@columbia.edu>
2021-01-28 18:48:42 -0600

    update install scripts

--- install/install-brew-cask.sh
+++ install/install-brew-cask.sh
@@ -8,20 +8,21 @@ apps=(
     brave-browser          # second browser to separate work/life
     dashlane               # password manager
     firefox                # keep it up to date!
-    flash-npapi            # adobe flash player
     github                 # can be helpful, I suck at git
     google-backup-and-sync # free storage from columbia
     iterm2                 # better terminal
     joplin                 # note taking
     julia-lts              # programming language!
     mactex-no-gui          # latex
+    mailspring             # email client
     mathpix-snipping-tool  # this is magic!
+    selfcontrol            # block distractions
     slack                  # lab group
     spotify                # why not
     the-unarchiver         # easier [un]zipping
     visual-studio-code     # code editor
     whatsapp               # benefits of having this on desktop are questionable
-    zoomus                 # we all work remotely now
+    zoom                   # we all work remotely now
     zotero                 # reference manager
 )
-brew cask install "${apps[@]}"
+brew install --cask "${apps[@]}"
