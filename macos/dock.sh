ee872a78ba48fc4fd907ba434a080d841eaf849e
James Doss-Gollin <jdossgollin@rice.edu>
2021-08-20 14:30:17 -0500

    no docker, add desktop to dock

--- macos/dock.sh
+++ macos/dock.sh
@@ -21,6 +21,7 @@ dockutil --no-restart --add "/Applications/iTerm.app"
 dockutil --no-restart --add "/Applications/Notion.app"
 dockutil --no-restart --add "~/Insync/jd82@rice.edu/Google Drive/"
 dockutil --no-restart --add "~/Documents/"
+dockutil --no-restart --add "~/Desktop/"
 dockutil --no-restart --add "~/Downloads/"
 
 killall Dock
