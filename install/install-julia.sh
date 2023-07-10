539149cfebf0f40eb4571a7661cb43f85ae135f2
James Doss-Gollin <jdossgollin@rice.edu>
2023-07-10 11:00:23 -0500

    Julia versions

--- install/install-julia.sh
+++ install/install-julia.sh
@@ -1,8 +1,7 @@
 # manual install
 brew install juliaup # recommended Julia installer
 
-juliaup add 1.6.7
-juliaup add 1.7.2~aarch64
+juliaup add 1.9
 juliaup add release
 juliaup default release
 juliaup update
